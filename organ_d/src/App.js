0671ebd58666e469d705d793aa58a7071e14a436
chamakaathukorala <s16316@sci.pdn.ac.lk>
2020-09-28 13:27:36 +0530

    react-router-dom added

--- organ_d/src/App.js
+++ organ_d/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, NavbarBrand, Jumbotron, Container } from 'reactstrap';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
