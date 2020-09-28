3ff691177c68ba4019b79e7ff592641323a2773b
chamakaathukorala <s16316@sci.pdn.ac.lk>
2020-09-28 13:45:43 +0530

    MainComponent and HeaderComponent

--- organ_d/src/App.js
+++ organ_d/src/App.js
@@ -1,30 +1,23 @@
-import React from 'react';
-import { Navbar, NavbarBrand, Jumbotron, Container } from 'reactstrap';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <Navbar dark color="danger">
-            <div className="container">
-              <NavbarBrand href="/">SLDonors</NavbarBrand>
-            </div>
-          </Navbar>
-        </div>
-      </header>
-      <Container>
-        <Jumbotron id="jumbotron" fluid>
-          <Container fluid>
-            <h1 className="display-3">Yes I'M A DONOR</h1>
-            <p className="display-5">Let's Give your Hand To Save An Another Life</p>
-          </Container>
-        </Jumbotron>
-      </Container>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <Main />
+      </div>
+      </BrowserRouter>
+    );
+  }
 }
+
+
+
+
 
 export default App;
