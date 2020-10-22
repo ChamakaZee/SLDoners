fa08af84ebbc01abd1334ae083de1ab72c60fc52
Chamaka Athukorala <s16316@sci.pdn.ac.lk>
2020-10-22 06:20:53 +0530

    Get users

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const organRoutes = express.Router();
+const userRoutes = express.Router();
 const PORT = 4000;
+
+let Organ = require('./models/organs.newItem');
+let User = require('./models/organs.newUser');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,6 +19,16 @@ const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })
+
+organRoutes.route('/').get(function(req, res) {
+    Organ.find(function(err, organs) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(organs);
+        }
+    });
+});
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
