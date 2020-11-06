cb719b18d5eb45e0b361981b35e684f39525b7d1
Chamaka Athukorala <s16316@sci.pdn.ac.lk>
2020-11-06 18:47:19 +0530

    remove reactstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
-import './App.css';
 
 class App extends Component {
 
@@ -15,9 +14,5 @@ class App extends Component {
     );
   }
 }
-
-
-
-
 
 export default App;
