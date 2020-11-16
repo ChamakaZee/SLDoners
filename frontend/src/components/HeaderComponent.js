d1fd088c03a536d356d87637b09e1a40d3a920a1
Chamaka Athukorala <s16316@sci.pdn.ac.lk>
2020-11-17 02:39:28 +0530

    fix header

--- frontend/src/components/HeaderComponent.js
+++ frontend/src/components/HeaderComponent.js
@@ -1,74 +1,38 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Container,Button } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isNavOpen: false
-    };
-    this.toggleNav = this.toggleNav.bind(this);//to get the functions is the same function
-  }
-
-  toggleNav() {
-    this.setState({
-      isNavOpen: !this.state.isNavOpen
-    });
-  }
-
   render() {
     return (
-      <React.Fragment>
-        <Navbar dark color="danger" expand="md">
-          <div className="container">
-            <NavbarToggler onClick={this.toggleNav} />
-            <NavbarBrand className="mr-auto" href="/">
-              SLDonors
-            </NavbarBrand>
-
-            <Collapse isOpen={this.state.isNavOpen} navbar>
-              <Nav navbar>
-                <NavItem>
-                  <NavLink className="nav-link" to="/home">
-                    <span className="fa fa-home fa-lg"></span> Home
-                </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="nav-link" to="/aboutus">
-                    <span className="fa fa-info fa-lg"></span> About Us
-                </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="nav-link" to="/contactus">
-                    <span className="fa fa-address-card fa-lg"></span> Contact Us
-                </NavLink>
-                </NavItem>
-              </Nav>
-              <Nav className='ml-auto nav-link' navbar>
-              <NavItem>
-              <NavLink className="nav-link" to="/login">
-                    <span className="fa fa-sign-in fa-lg"></span>Login
-              </NavLink>
-              </NavItem>
-            </Nav>
-            </Collapse>
-          </div>
-        </Navbar>
-        <Container>
-          <Jumbotron>
-            <div className="container">
-              <div className="row row-header">
-                <div className="col-12 col-sm-6">
-                  <h1>Yes I,M A DONOR</h1>
-                  <p>Let's Save An Another Life</p>
-                </div>
-              </div>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <h6 class="navbar-brand" target="_blank">
+              <img src={logo5} width="100" height="50" alt="#" />
+            </h6>
+            <h3>Helping Hand Foundation <h5>Donate Your Organ , You Could Save Many Lives</h5></h3>
+          
+            </nav>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Donate Organs</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/list" className="nav-link">Request Organs</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/info" className="nav-link">How Donation Works</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/contact" className="nav-link">Contact Us</Link>
+                </li>
+              </ul>
             </div>
-          </Jumbotron>
-        </Container>
-      </React.Fragment>
+          </nav>
+          <br/>
+          <div><img src={mainImg} alt="organs"/></div>
+        </div>
     );
   }
 }
