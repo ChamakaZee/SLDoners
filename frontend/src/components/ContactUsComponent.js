0ac50d276987b4677054c174adc9872c5fed2c81
Chamaka Athukorala <s16316@sci.pdn.ac.lk>
2020-11-19 20:59:02 +0530

    Fix navigation

--- frontend/src/components/ContactUsComponent.js
+++ frontend/src/components/ContactUsComponent.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
-//import { Map, GoogleApiWrapper } from 'google-maps-react';
-
 import mainImg from "../images/main.JPG";
 import aboutImg from "../images/about_us_header.png";
 import helpImg from "../images/help.png";
 
-export default class Contact extends Component {
+export default class ContactUsComponent extends Component {
   constructor(props) {
     super(props);
   }
@@ -15,26 +12,6 @@ export default class Contact extends Component {
     return (
       <div style={{ marginTop: 10 }} class="background-color-main">
         <div className="container" class="background-color-main">
-          <h6>
-            <nav className="navbar navbar-expand-lg navbar-light">
-              <div className="collpase navbar-collapse">
-                <ul className="nav nav-pills nav-fil">
-                  <li className="nav-item">
-                    <Link to="/donate" className="nav-link text-primary">Donate Organs</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/list" className="nav-link text-primary">Request Organs</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/info" className="nav-link text-primary">How Donation Works</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/contact" className="nav-link text-primary">Contact Us</Link>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          </h6>
           <div><img src={mainImg} class="img-fluid" alt="Responsive image" /></div>
           <br></br>
           <h6>About ...</h6>
@@ -54,12 +31,6 @@ export default class Contact extends Component {
           <p>Email : helpingHand@support.com</p>
           <p></p>
           <div class="text-center"><img src={helpImg} class="img-fluid" alt="Responsive image" /></div>
-          {/* <Map
-                    google={this.props.google}
-                    zoom={8}
-                    style={mapStyles}
-                    initialCenter={{ lat: 47.444, lng: -122.176}}
-                    /> */}
         </div>
       </div>
     )
